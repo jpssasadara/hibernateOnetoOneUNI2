c464c35c7f89a64d12849b8b68c77f61a4bfd407
jps sasadara <jpssasadara1995@gmail.com>
2019-09-08 21:01:21 +0530

    my blog

---
+++ hb-04-one-to-many-uni/src/com/sasadara/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -0,0 +1,73 @@
+package com.sasadara.hibernate.demo;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import sasadara.hibernate.demo.entity.Course;
+import sasadara.hibernate.demo.entity.Instructor;
+import sasadara.hibernate.demo.entity.InstructorDetail;
+import sasadara.hibernate.demo.entity.Review;
+
+public class CreateCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			
+			// create a course
+			//below is one also ok since foreign key can be null 
+			//Course tempCourse = new Course("Pacman - How To Score One Million Points2");
+			int theId = 12;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			// add some reviews
+			tempCourse.addReview(new Review("Great course ... loved it!3"));
+			tempCourse.addReview(new Review("Cool course, job well done3"));
+			tempCourse.addReview(new Review("What a dumb course, you are an idiot!3"));
+						
+			// save the course ... and leverage the cascade all :-)
+			System.out.println("Saving the course");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			
+			session.save(tempCourse);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
