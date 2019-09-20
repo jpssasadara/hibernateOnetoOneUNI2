53bd2206021050f333b7a8cb14624e8d5f005e65
j.p.s sasadara <jpssasadara1995@gmail.com>
2019-09-20 09:35:10 +0530

    Update TennisCoach.java
    change

--- Spring-demo-anotation/src/com/luv2code/springdemo/TennisCoach.java
+++ Spring-demo-anotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,9 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
-
+/*1. @Component Annotation
+The @Component annotation marks a java class as a bean so the component-scanning mechanism-
+of spring can pick it up and pull it into the application context. To use this annotation, apply it over class as below:*/
 @Component
 public class TennisCoach implements Coach {
 
